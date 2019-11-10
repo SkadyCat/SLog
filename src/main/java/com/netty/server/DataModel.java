50db0b46ff2d87ccfb01f094b0a980874a4aa862
ljy <874212844@qq.com>
2019-11-10 16:53:38 +0800

    BR:2.02

--- src/main/java/com/netty/server/DataModel.java
+++ src/main/java/com/netty/server/DataModel.java
@@ -47,7 +47,7 @@ public class DataModel {
 
             modelStatus = -1;
             System.out.println("异常帧，不执行byte长度 =:"+bytes.length+"<字符长度 = >"+
-                    va+"float长度 = ");
+                    va+"float长度 = "+"----"+new String(bytes));
             return;
         }
 
